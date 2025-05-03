c19a78c701d0edf4942756080a5c903a607cd4ce
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2025-05-03 11:53:23 -0400

    No need of humctl tool
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>

--- .devcontainer/installMoreTools.sh
+++ .devcontainer/installMoreTools.sh
@@ -15,12 +15,6 @@ tar -xvf score-k8s_${SCORE_K8S_VERSION}_linux_amd64.tar.gz
 chmod +x score-k8s
 sudo mv score-k8s /usr/local/bin
 
-HUMCTL_VERSION=$(curl -sL https://api.github.com/repos/humanitec/cli/releases/latest | jq -r .tag_name)
-curl -fLO https://github.com/humanitec/cli/releases/download/${HUMCTL_VERSION}/cli_${HUMCTL_VERSION:1}_linux_amd64.tar.gz
-tar -xvf cli_${HUMCTL_VERSION:1}_linux_amd64.tar.gz
-chmod +x humctl
-sudo mv humctl /usr/local/bin/humctl
-
 KIND_VERSION=$(curl -sL https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r .tag_name)
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-linux-amd64
 chmod +x ./kind
