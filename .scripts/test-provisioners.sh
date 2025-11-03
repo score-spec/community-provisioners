fba8562e84f5f52d797b0b6a1e912e6066ac18b3
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2025-11-02 23:24:39 -0500

    Microcks (#27)
    * Create 00-endpoint-with-microcks.compose.provisioners.yaml
    
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>
    
    * Create 00-endpoint-with-microcks-cli.k8s.provisioners.yaml
    
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>
    
    * Rename 00-endpoint-with-microcks.compose.provisioners.yaml to 00-endpoint-with-microcks.provisioners.yaml
    
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>
    
    * Rename 00-endpoint-with-microcks-cli.k8s.provisioners.yaml to 00-endpoint-with-microcks-cli.provisioners.yaml
    
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>
    
    * Update endpoint with Microcks to latest version
    
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>
    
    * Update test-provisioners.sh to skip folders different than score-*
    
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>
    
    ---------
    
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>

--- .scripts/test-provisioners.sh
+++ .scripts/test-provisioners.sh
@@ -9,19 +9,25 @@ do
   implementations=$(ls -d */)
   for implementation in $implementations;
   do
-    echo "#### For ${implementation%?}:"
-    provisioners=$(ls $implementation*.provisioners.yaml)
-    for provisioner in $provisioners;
-    do
-      echo "###### With ${provisioner}:"
-      if [[ "$provisioner" = "score-compose/10-dns-in-codespace.provisioners.yaml" || "$provisioner" = "score-k8s/10-dns-in-codespace.provisioners.yaml" || "$provisioner" = "score-compose/10-dmr-llm-model-via-curl-cmd.provisioners.yaml" ]]; then
-        echo "Skipped."
-      else
-        ${implementation%?} init --no-sample --provisioners $provisioner
-        ${implementation%?} generate $(ls score*.yaml)
-        rm -r .${implementation%?}
-      fi
-    done
+    if [[ "$implementation" = "score-compose" 
+      || "$implementation" = "score-k8s" ]]; then
+      echo "#### For ${implementation%?}:"
+      provisioners=$(ls $implementation*.provisioners.yaml)
+      for provisioner in $provisioners;
+      do
+        echo "###### With ${provisioner}:"
+        if [[ "$provisioner" = "score-compose/10-dns-in-codespace.provisioners.yaml" 
+        || "$provisioner" = "score-k8s/10-dns-in-codespace.provisioners.yaml" 
+        || "$provisioner" = "score-compose/10-dmr-llm-model-via-curl-cmd.provisioners.yaml"
+        || "$provisioner" = "score-k8s/10-endpoint-with-microcks-cli.provisioners.yaml" ]]; then
+          echo "Skipped."
+        else
+          ${implementation%?} init --no-sample --provisioners $provisioner
+          ${implementation%?} generate $(ls score*.yaml)
+          rm -r .${implementation%?}
+        fi
+      done
+    fi
   done
   cd ..
 done
